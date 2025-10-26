3a44e0cacac73cc78d5111bed11fc98be3842899
Komal Harshita <komal.sony234@gmail.com>
2025-10-26 20:16:04 +0530

    add random fact generator file

---
+++ scripts/random_fact_generator.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def get_fact():
+    response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    data = json.loads(response.text)  
+    fact = data['text']        
+    return fact
+
+print("---Welcome to my random fact generator---\n")
+
+print("Enter a number: ")
+n = int(input())
+
+if n > 7:
+    print("Maximum number of facts per execution is 7")
+elif n <= 0:
+    print("Minimum number of facts per execution is 1")
+else:
+    for i in range(n):
+        print(get_fact())
+        print()
